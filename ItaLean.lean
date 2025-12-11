23d9a6d2756227f3c0457a2b2c99b632848ee19c
Damiano Testa <adomani@gmail.com>
2025-12-11 01:05:38 +0100

    mk_all

--- ItaLean.lean
+++ ItaLean.lean
@@ -1,3 +1,8 @@
+import ItaLean.Analysis
+import ItaLean.Analysis_solutions
+import ItaLean.Damiano.Inspect
+import ItaLean.Damiano.Metaprogramming
+import ItaLean.Damiano.ShadyAuxLemmas
 import ItaLean.Intro
 import ItaLean.Riccardo
 import ItaLean.Test
